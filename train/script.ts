f85350120cd04824efc22f368d182db5b8d14f50
invalid w <wangjuesix@gmail.com>
2022-01-18 11:07:57 +0800

    feat: 增加激活函数 隐藏层

--- train/script.ts
+++ train/script.ts
@@ -1,7 +1,11 @@
 import * as tf from "@tensorflow/tfjs"
 import * as tfvis from "@tensorflow/tfjs-vis"
 import saleDataset from "./sale"
-
+// const saleDataset = [
+//     {rank:150, sale: 40},
+//     {rank:160, sale: 50},
+//     {rank:170, sale: 60},
+// ]
 
 window.onload = async () => {
     plot()
@@ -10,25 +14,26 @@ window.onload = async () => {
 
 
     const model = tf.sequential()
-    model.add(tf.layers.dense({ units: 1, inputShape: [1] }))
-    model.compile({ loss: tf.losses.meanSquaredError, optimizer: tf.train.sgd(0.00001) })
-
+    model.add(tf.layers.dense({ units: 1, inputShape: [1], activation: 'relu' }))
+    model.add(tf.layers.dense({
+        units: 1,
+        activation: 'sigmoid'
+    }))
+    model.compile({ loss: tf.losses.meanSquaredError, optimizer: tf.train.sgd(0.1) })
+    // model.compile({
+    //     loss: tf.losses.logLoss,
+    //     optimizer: tf.train.adam(0.1)
+    // })
     const [inputs, inputSubVal, inputMin] = normalize(tf.tensor(xs))
     const [labels, labelSubVal, labelMin] = normalize(tf.tensor(ys))
    
     await model.fit(inputs, labels, {
         batchSize: saleDataset.length,
-        epochs: 10,
+        epochs: 300,
         callbacks: tfvis.show.fitCallbacks({ name: '训练过程', }, ['loss'])
     })
 
-    const predictRank = 10
-    const output = model.predict(
-        tf.tensor([predictRank]).sub(inputMin).div(inputSubVal)
-    ) as tf.Tensor
-
-
-    console.log('输入排名为', predictRank, '预测结果为',output.mul(labelSubVal).add(labelMin).dataSync()[0])
+    predict(model, inputMin, inputSubVal, labelSubVal, labelMin)
     
 }
 function plot(){
@@ -48,8 +53,21 @@ function normalize(tensor: tf.Tensor):[tf.Tensor, tf.Tensor, tf.Tensor] {
     const max = tensor.max()
     const subVal = max.sub(min)
     return [
-        tensor.div(subVal),
+        tensor.sub(min).div(subVal),
         subVal,
         min
     ]
+}
+
+
+function predict(model: tf.Sequential, inputMin: tf.Tensor, inputSubVal: tf.Tensor, labelSubVal: tf.Tensor, labelMin: tf.Tensor,){
+    const predictRank = 100
+    const output = model.predict(
+        tf.tensor([predictRank]).sub(inputMin).div(inputSubVal)
+    ) as tf.Tensor
+
+
+    console.log('输入排名为', predictRank, 
+    '预测结果为',
+    output.mul(labelSubVal).add(labelMin).dataSync()[0], '实际结果为 240')
 }
